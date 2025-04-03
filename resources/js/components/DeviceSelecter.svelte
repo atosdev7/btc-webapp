cf48f48610bf9bb70798e39923ddfa88743585c3
atosdev7 <atosdev7@gmail.com>
2025-04-04 00:30:34 +0900

    add device selector label

--- resources/js/components/DeviceSelecter.svelte
+++ resources/js/components/DeviceSelecter.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import * as Select from '@/components/ui/select/index.js';
+    import { Label } from '@/components/ui/label'
 
     interface Device {
         id: number;
@@ -19,20 +20,33 @@
     let triggerContent = $derived(devices.find((device) => device.id == selectedDeviceId)?.name ?? 'Select a device');
 </script>
 
-<Select.Root type="single" name="selectedDevice" bind:value={selectedDeviceId} onValueChange={(value) => {
-        if (value) {
-            selectedDeviceId = Number(value);
-            if (onSelect) {
-                onSelect(selectedDeviceId);
-            }
+<style>
+    @media (max-width: 480px) { /* Tailwind's sm breakpoint */
+        .hidden-mobile {
+            display: none !important;
         }
-    }}>
-    <Select.Trigger class="w-[180px]">
-        {triggerContent}
-    </Select.Trigger>
-    <Select.Content>
-        {#each devices as device}
-            <Select.Item value={String(device.id)} label={String(device.board_id)}>{device.name}</Select.Item>
-        {/each}
-    </Select.Content>
-</Select.Root>
+    }
+</style>
+
+<div class="flex gap-2 items-center">
+    <div class="hidden-mobile">
+        <Label class="hidden-mobile">Device: </Label>
+    </div>
+    <Select.Root type="single" name="selectedDevice" bind:value={selectedDeviceId} onValueChange={(value) => {
+            if (value) {
+                selectedDeviceId = Number(value);
+                if (onSelect) {
+                    onSelect(selectedDeviceId);
+                }
+            }
+        }}>
+        <Select.Trigger class="w-[180px]">
+            {triggerContent}
+        </Select.Trigger>
+        <Select.Content>
+            {#each devices as device}
+                <Select.Item value={String(device.id)} label={String(device.board_id)}>{device.name}</Select.Item>
+            {/each}
+        </Select.Content>
+    </Select.Root>
+</div>
